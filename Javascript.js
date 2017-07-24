ede3a6bdf63fd8f9ed7d0ce0daf16addfc88474d
Austinparisi42 <austinparisi1991@gmail.com>
2017-07-23 23:09:15 -0500

    Add files via upload
    Sorry if this is disorganized. I didn't know how to get the actual folders that all my work are saved to onto github.

---
+++ Javascript.js
@@ -0,0 +1,30 @@
+<script type="text/javascript">
+
+        document.getElementById("button1").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "250px";
+
+        });
+
+        document.getElementById("button2").addEventListener("click", function(){
+
+            document.getElementById("box").style.color = "blue";
+
+        });
+
+        document.getElementById("button3").addEventListener("click", function(){
+
+            document.getElementById("box").style.opacity = "0.5";
+
+        });
+
+        document.getElementById("button4").addEventListener("click", function(){
+
+            document.getElementById("box").style.opacity = "0.5"; style.height = "150px"; style.color = "orange";
+
+        });
+
+
+
+
+    </script>
